c37c05425547d68e8f0d2f4d8312d7c9efdcec69
IsJustaGhost <56104601+IsJustaGhost@users.noreply.github.com>
2021-10-24 07:55:52 -0700

    Added UI to /lftestfilters

--- LibFilters-3.0/test.lua
+++ LibFilters-3.0/test.lua
@@ -2,6 +2,7 @@
 local libFilters = LibFilters3
 if not libFilters then return end
 libFilters:InitializeLibFilters()
+ 
 
 ------------------------------------------------------------------------------------------------------------------------
 -- HELPER VARIABLES AND FUNCTIONS FOR TESTS
@@ -48,114 +49,299 @@ end
 ------------------------------------------------------------------------------------------------------------------------
 -- Custom SLASH COMMANDS for tests
 ------------------------------------------------------------------------------------------------------------------------
-SLASH_COMMANDS["/lftestfilters"] = function()
-	local filterTypes = {
-		LF_INVENTORY,
-		LF_INVENTORY_QUEST,
-		LF_CRAFTBAG,
-		LF_BANK_WITHDRAW,
-		LF_BANK_DEPOSIT,
-		LF_GUILDBANK_WITHDRAW,
-		LF_GUILDBANK_DEPOSIT,
-		LF_VENDOR_BUY,
-		LF_VENDOR_SELL,
-		LF_VENDOR_BUYBACK,
-		LF_VENDOR_REPAIR,
-		LF_GUILDSTORE_SELL,
-		LF_MAIL_SEND,
-		LF_TRADE,
-		LF_SMITHING_REFINE,
-		LF_SMITHING_DECONSTRUCT,
-		LF_SMITHING_IMPROVEMENT,
-		LF_SMITHING_RESEARCH,
-		LF_ALCHEMY_CREATION,
-		LF_ENCHANTING_CREATION,
-		LF_ENCHANTING_EXTRACTION,
-		LF_FENCE_SELL,
-		LF_FENCE_LAUNDER,
-		LF_QUICKSLOT,
-		LF_RETRAIT,
-		LF_HOUSE_BANK_WITHDRAW,
-		LF_HOUSE_BANK_DEPOSIT,
-		LF_JEWELRY_REFINE,
-		LF_JEWELRY_CREATION,
-		LF_JEWELRY_DECONSTRUCT,
-		LF_JEWELRY_IMPROVEMENT,
-		LF_JEWELRY_RESEARCH,
-		LF_SMITHING_RESEARCH_DIALOG,
-		LF_JEWELRY_RESEARCH_DIALOG,
-		LF_INVENTORY_COMPANION
-	}
+local filterTypes = {
+	LF_INVENTORY,
+	LF_INVENTORY_QUEST,
+	LF_CRAFTBAG,
+	LF_BANK_WITHDRAW,
+	LF_BANK_DEPOSIT,
+	LF_GUILDBANK_WITHDRAW,
+	LF_GUILDBANK_DEPOSIT,
+	LF_VENDOR_BUY,
+	LF_VENDOR_SELL,
+	LF_VENDOR_BUYBACK,
+	LF_VENDOR_REPAIR,
+	LF_GUILDSTORE_SELL,
+	LF_MAIL_SEND,
+	LF_TRADE,
+	LF_SMITHING_REFINE,
+	LF_SMITHING_DECONSTRUCT,
+	LF_SMITHING_IMPROVEMENT,
+	LF_SMITHING_RESEARCH,
+	LF_ALCHEMY_CREATION,
+	LF_ENCHANTING_CREATION,
+	LF_ENCHANTING_EXTRACTION,
+	LF_FENCE_SELL,
+	LF_FENCE_LAUNDER,
+	LF_QUICKSLOT,
+	LF_RETRAIT,
+	LF_HOUSE_BANK_WITHDRAW,
+	LF_HOUSE_BANK_DEPOSIT,
+	LF_JEWELRY_REFINE,
+	LF_JEWELRY_CREATION,
+	LF_JEWELRY_DECONSTRUCT,
+	LF_JEWELRY_IMPROVEMENT,
+	LF_JEWELRY_RESEARCH,
+	LF_SMITHING_RESEARCH_DIALOG,
+	LF_JEWELRY_RESEARCH_DIALOG,
+	LF_INVENTORY_COMPANION
+}
 
+local enabledFilters = {}
+test = {}
+local LIST_TYPE = 1
+local enableList = {}
+local updateList = {}
 
-	local function doesItemPassFilter(bagId, slotIndex, stackCount)
-		local itemType,  specializedItemType = GetItemType(bagId, slotIndex)
-		local quality = GetItemQuality(bagId, slotIndex)
+local function doesItemPassFilter(bagId, slotIndex, stackCount)
+	local itemType,  specializedItemType = GetItemType(bagId, slotIndex)
+	local quality = GetItemQuality(bagId, slotIndex)
 
-		if itemType == ITEMTYPE_ENCHANTING_RUNE_ASPECT then
-			return quality < ITEM_FUNCTIONAL_QUALITY_ARCANE
-		elseif itemType == ITEMTYPE_BLACKSMITHING_BOOSTER then
-			return quality < ITEM_FUNCTIONAL_QUALITY_ARCANE
-		elseif itemType == ITEMTYPE_CLOTHIER_BOOSTER then
-			return quality < ITEM_FUNCTIONAL_QUALITY_ARCANE
-		elseif itemType == ITEMTYPE_JEWELRYCRAFTING_BOOSTER then
-			return quality < ITEM_FUNCTIONAL_QUALITY_ARCANE
-		elseif itemType == ITEMTYPE_WOODWORKING_BOOSTER then
-			return quality < ITEM_FUNCTIONAL_QUALITY_ARCANE
-		elseif itemType == ITEMTYPE_WEAPON or itemType == ITEMTYPE_ARMOR then
-			return quality < ITEM_FUNCTIONAL_QUALITY_ARCANE and not IsItemPlayerLocked(bagId, slotIndex) and
-				GetItemActorCategory(bagId, slotIndex) ~= GAMEPLAY_ACTOR_CATEGORY_COMPANION
-		elseif itemType == ITEMTYPE_POISON_BASE or itemType == ITEMTYPE_POTION_BASE or itemType == ITEMTYPE_REAGENT then
-			return stackCount > 100
-		end
-
-		if quality > ITEM_FUNCTIONAL_QUALITY_ARCANE then
-			return false
-		end
-		return stackCount > 1
+	if itemType == ITEMTYPE_ENCHANTING_RUNE_ASPECT then
+		return quality < ITEM_FUNCTIONAL_QUALITY_ARCANE
+	elseif itemType == ITEMTYPE_BLACKSMITHING_BOOSTER then
+		return quality < ITEM_FUNCTIONAL_QUALITY_ARCANE
+	elseif itemType == ITEMTYPE_CLOTHIER_BOOSTER then
+		return quality < ITEM_FUNCTIONAL_QUALITY_ARCANE
+	elseif itemType == ITEMTYPE_JEWELRYCRAFTING_BOOSTER then
+		return quality < ITEM_FUNCTIONAL_QUALITY_ARCANE
+	elseif itemType == ITEMTYPE_WOODWORKING_BOOSTER then
+		return quality < ITEM_FUNCTIONAL_QUALITY_ARCANE
+	elseif itemType == ITEMTYPE_WEAPON or itemType == ITEMTYPE_ARMOR then
+		return quality < ITEM_FUNCTIONAL_QUALITY_ARCANE and not IsItemPlayerLocked(bagId, slotIndex) and
+			GetItemActorCategory(bagId, slotIndex) ~= GAMEPLAY_ACTOR_CATEGORY_COMPANION
+	elseif itemType == ITEMTYPE_POISON_BASE or itemType == ITEMTYPE_POTION_BASE or itemType == ITEMTYPE_REAGENT then
+		return stackCount > 100
 	end
 
-	for _, filterType in pairs(filterTypes) do
-		libFilters.test[filterType] = {}
+	if quality > ITEM_FUNCTIONAL_QUALITY_ARCANE then
+		return false
+	end
+	return stackCount > 1
+end
 
-		local function filterCallback(slotOrBagId, slotIndex)
-			if slotIndex then
-				local bagId = slotOrBagId
-				local itemLink = GetItemLink(bagId, slotIndex)
-				local stackCountBackpack, stackCountBank, stackCountCraftBag = GetItemLinkStacks(itemLink)
-				local stackCount = stackCountBackpack + stackCountBank + stackCountCraftBag
+local function registerFilter(filterType, filterTypeName)
+	local function filterBagIdAndSlotIndexCallback(bagId, slotIndex)
+		local itemLink = GetItemLink(bagId, slotIndex)
+		local stackCountBackpack, stackCountBank, stackCountCraftBag = GetItemLinkStacks(itemLink)
+		local stackCount = stackCountBackpack + stackCountBank + stackCountCraftBag
 
-				local result = doesItemPassFilter(bagId, slotIndex, stackCount)
-				if result == false then
-					-- can take a moment to display for research, has a low filter threshold
-					d(string.format("--	test, filterType:( %s ), stackCount:( %s ), itemLink: %s", filterType, stackCount, itemLink))
-				end
-				return result
-			else
-				local bagId, slotIndex = slotOrBagId.bagId, slotOrBagId.slotIndex
-				local itemLink = bagId == nil and GetQuestItemLink(slotIndex) or GetItemLink(bagId, slotIndex)
-				local stackCountBackpack, stackCountBank, stackCountCraftBag = GetItemLinkStacks(itemLink)
-				local stackCount = stackCountBackpack + stackCountBank + stackCountCraftBag
-
-				local result = doesItemPassFilter(bagId, slotIndex, stackCount)
-				if result == false then
-					-- can take a moment to display for research, has a low filter threshold
-					d(string.format("--	test, filterType:( %s ), stackCount:( %s ), itemLink: %s", filterType, stackCount, itemLink))
-				end
-				return result
-			end
+		local result = doesItemPassFilter(bagId, slotIndex, stackCount)
+		if result == false then
+			-- can take a moment to display for research, has a low filter threshold
+			d(string.format("--	test, filterType:( %s ), stackCount:( %s ), itemLink: %s", filterType, stackCount, itemLink))
 		end
+		return result
+	end
 
+	local function filterSlotDataCallback(slotData)
+		local bagId, slotIndex = slotData.bagId, slotData.slotIndex
+		local itemLink = bagId == nil and GetQuestItemLink(slotIndex) or GetItemLink(bagId, slotIndex)
+		local stackCountBackpack, stackCountBank, stackCountCraftBag = GetItemLinkStacks(itemLink)
+		local stackCount = stackCountBackpack + stackCountBank + stackCountCraftBag
+
+		local result = doesItemPassFilter(bagId, slotIndex, stackCount)
+		if result == false then
+			-- can take a moment to display for research, has a low filter threshold
+			d(string.format("--	test, filterType:( %s ), stackCount:( %s ), itemLink: %s", filterType, stackCount, itemLink))
+		end
+		return result
+	end
+	
+	local usingBagAndSlot = libFilters.constants.filterTypes.UsingBagIdAndSlotIndexFilterFunction[filterType]
+	
+	d("[LibFilters3]Registering " .. filterTypeName)
+	if usingBagAndSlot then
+		libFilters:RegisterFilter(filterTag, filterType, filterBagIdAndSlotIndexCallback)
+	else
+		libFilters:RegisterFilter(filterTag, filterType, filterSlotDataCallback)
+	end
+end
+
+local function refresh(dataList)
+	for _, filterType in pairs(filterTypes) do
+		local isRegistered = libFilters:IsFilterRegistered(filterTag, filterType)
 		local filterTypeName = libFilters:GetFilterTypeName(filterType)
-		if libFilters:IsFilterRegistered(filterTag, filterType) then
+		
+		if enabledFilters[filterType] then
+			local data = {
+				['filterType'] = filterType,
+				['name'] = filterTypeName
+			}
+			table.insert(dataList, ZO_ScrollList_CreateDataEntry(LIST_TYPE, data))
+			if not isRegistered then
+				registerFilter(filterType, filterTypeName)
+			end
+		elseif isRegistered then
 			d("[LibFilters3]Unregistering " .. filterTypeName)
 			libFilters:UnregisterFilter(filterTag, filterType)
-		else
-			d("[LibFilters3]Registering " .. filterTypeName)
-			libFilters:RegisterFilter(filterTag, filterType, filterCallback)
 		end
- 	    libFilters:RequestUpdate(filterType)
 	end
+end
+
+local function performFullRefresh()
+	ZO_ScrollList_Clear(updateList)
+	refresh(ZO_ScrollList_GetDataList(updateList))
+	ZO_ScrollList_Commit(updateList)
+	
+	-- changing hidden state to make the scrollbar show. Otherwise updateList's scrollbar will not show 
+	-- if the list was not populated with enough itmes prior to enabling /lftestfilters
+	LibFilters3_Test_TLW:SetHidden(true)
+	LibFilters3_Test_TLW:SetHidden(false) 
+end
+
+local function refreshEnableList()
+	ZO_ScrollList_Clear(enableList)
+	local dataList = ZO_ScrollList_GetDataList(enableList)
+	for _, filterType in pairs(filterTypes) do
+		local filterTypeName = libFilters:GetFilterTypeName(filterType)
+		local data = {
+			['filterType'] = filterType,
+			['name'] = filterTypeName
+		}
+		
+		local newData = ZO_ScrollList_CreateDataEntry(LIST_TYPE, data)
+		table.insert(dataList, newData)
+	end
+	ZO_ScrollList_Commit(enableList)
+end
+
+local function clearAll()
+	for filterType, bool in pairs(enabledFilters) do
+		if bool then
+			enabledFilters[filterType] = false
+		end
+	end
+	refreshEnableList()
+	performFullRefresh()
+end
+
+local function intialize()
+	local width, height = GuiRoot:GetDimensions()
+	local adjustedHeight = (height * 0.75)
+	local y_Adj = (height - adjustedHeight) / 2
+	
+    local tlw = CreateTopLevelWindow("LibFilters3_Test_TLW")
+	tlw:SetHidden(true)
+	
+	local tlc = CreateControl("LibFilters3_Test_TLC", tlw, CT_TOPLEVELCONTROL)
+	tlc:SetMouseEnabled(true)
+	tlc:SetMovable(true)
+	tlc:SetClampedToScreen(true)
+	tlc:SetDimensions(350, adjustedHeight)
+	tlc:SetAnchor(TOPRIGHT, GuiRoot, TOPRIGHT, nil, y_Adj)
+    local backdrop = CreateControlFromVirtual("LibFilters3_Test_TLCBg", tlc, "ZO_DefaultBackdrop")	
+	backdrop:SetAnchorFill()
+	
+	-- create main LF_constants list
+	-- this list is used to enable/disable LF_constants filters
+	enableList = CreateControlFromVirtual("$(parent)EnableList", tlc, "ZO_ScrollList")
+	enableList:SetDimensions(345, adjustedHeight * 0.5)
+	enableList:SetAnchor(TOPLEFT, tlc, nil, 0, 25)
+	
+--	/lftestfilters
+	-- button container for clear and refresh
+	local buttons = CreateControl("$(parent)Buttons", tlc, CT_CONTROL)	
+	buttons:SetDimensions(340, 60)
+	buttons:SetAnchor(TOP, enableList, BOTTOM, 3, 15)
+    local buttonshBackdrop = CreateControlFromVirtual("LibFilters3_Test_TLCbuttonsBg", buttons, "ZO_DefaultBackdrop")
+	buttonshBackdrop:SetAnchorFill()
+	
+	-- create Clear button
+	-- resets enableList and clears updateList
+	local btnClear = CreateControlFromVirtual("$(parent)ClearButton", buttons, "ZO_DefaultButton")
+	btnClear:SetHidden(false)
+	btnClear:SetText("Clear")
+	btnClear:SetDimensions(100, 50)
+	btnClear:SetAnchor(TOPLEFT, buttons, TOPLEFT, 10, 5)
+	btnClear:SetHandler("OnMouseUp", function(btn)
+		clearAll()
+	end)
+	
+	-- create Refresh button
+	-- refresh list of LF_constants update functions and enable/disable LF_constants filters based on selections in enableList
+	local btnRefresh = CreateControlFromVirtual("$(parent)RefreshButton", buttons, "ZO_DefaultButton")
+	btnRefresh:SetHidden(false)
+	btnRefresh:SetText("Refresh")
+	btnRefresh:SetDimensions(150, 50)
+	btnRefresh:SetAnchor(TOPRIGHT, buttons, TOPRIGHT, -10, 5)
+	btnRefresh:SetHandler("OnMouseUp", function(btn)
+		performFullRefresh()
+	end)
+	
+	-- create list for LF_constants update functions
+	local ul_Hight = tlc:GetBottom() -  buttons:GetBottom() - 20
+	updateList = CreateControlFromVirtual("$(parent)UpdateList", tlc, "ZO_ScrollList")
+	updateList:SetDimensions(345, ul_Hight)
+	updateList:SetAnchor(TOP, buttons, BOTTOM, 0, 15)
+	
+	-- initialize lists
+	ZO_ScrollList_Initialize(enableList)
+	ZO_ScrollList_Initialize(updateList)
+	
+--	LibFilters3_Test_TLC:SetHidden(true) 
+end
+
+local function setupUsableAndLockedColor(control, locked)
+	if locked then
+		control:SetAlpha(0.3)
+	else
+		control:SetAlpha(1)
+	end
+end
+local function setupRow(rowControl, data, onMouseUp)
+	rowControl.data = data
+	local row = rowControl:GetNamedChild("Button")
+	row:SetText(data.name)
+	
+	rowControl:SetHidden(false)
+	setupUsableAndLockedColor(rowControl, not enabledFilters[data.filterType])
+	
+	row:SetHandler("OnMouseUp", onMouseUp)
+end
+
+local function addListDataTypes()
+ 	local function setupEnableRow(rowControl, data)
+		setupRow(rowControl, data, function(btn)
+			setupUsableAndLockedColor(rowControl, enabledFilters[data.filterType])
+			enabledFilters[data.filterType] = not enabledFilters[data.filterType]
+		end)
+	end
+
+ 	local function setupUpdateRow(rowControl, data)
+		setupRow(rowControl, data, function(btn)
+			libFilters:RequestUpdate(data.filterType)
+		end)
+	end
+	
+	ZO_ScrollList_AddDataType(enableList, LIST_TYPE, "LibFilters3_Test_Template", 50, setupEnableRow)
+	ZO_ScrollList_AddDataType(updateList, LIST_TYPE, "LibFilters3_Test_Template", 50, setupUpdateRow)
+end
+
+
+SLASH_COMMANDS["/lftestfilters"] = function()
+--[[ is there a way to check if a virtual control exists?
+	-- is test.xml enabled
+	if not LibFilters3_Test_Template then
+		d( "Enable test.xml in manifest")
+		return
+	end
+]]
+
+	if not LibFilters3_Test_TLW then
+		intialize()
+		addListDataTypes()
+	end
+
+	if not LibFilters3_Test_TLC:IsHidden() then 
+		LibFilters3_Test_TLW:SetHidden(true)
+		ZO_ScrollList_Clear(enableList)
+		ZO_ScrollList_Commit(enableList)
+		return 
+	end
+	LibFilters3_Test_TLW:SetHidden(false)
+
+	refreshEnableList(ZO_ScrollList_GetDataList(enableList))
 end
 
 --depends on Item Saver by Randactyl
@@ -249,3 +435,4 @@ end
 SLASH_COMMANDS["/lftesthousebankwithdraw"] = function()
 	toggleFilterForFilterType(LF_HOUSE_BANK_WITHDRAW)
 end
+
